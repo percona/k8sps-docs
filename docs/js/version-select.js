8e2310b9a58b989eacca9aefb18e3b4613d5ce83
Dima <dmitriykostiuk@gmail.com>
2022-09-26 00:16:08 +0300

    version switcher fix

--- docs/js/version-select.js
+++ docs/js/version-select.js
@@ -2,26 +2,24 @@
  * Custom version of same taken from mike code for injecting version switcher into percona.com
  */
 
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("DOMContentLoaded", function() {
   // This is a bit hacky. Figure out the base URL from a known CSS file the
   // template refers to...
-  var ex = new RegExp('/?css/version-select.css$');
+  var ex = new RegExp("/?css/version-select.css$");
   var sheet = document.querySelector('link[href$="version-select.css"]');
 
-  if (!sheet) {
-    return;
-  }
-
-  var ABS_BASE_URL = sheet.href.replace(ex, '');
-  var CURRENT_VERSION = ABS_BASE_URL.split('/').pop();
+  var ABS_BASE_URL = sheet.href.replace(ex, "");
+  var ABS_BASE_URL_PART = ABS_BASE_URL.substring(0, ABS_BASE_URL.lastIndexOf("/"));
+  var CURRENT_VERSION = ABS_BASE_URL.split("/").pop();
 
   function makeSelect(options, selected) {
-    var select = document.createElement('select');
-    select.classList.add('btn');
-    select.classList.add('btn-primary');
+    var select = document.createElement("select");
+    select.classList.add("btn");
+    select.classList.add("btn-primary");
 
-    options.forEach(function (i) {
-      var option = new Option(i.text, i.value, undefined, i.value === selected);
+    options.forEach(function(i) {
+      var option = new Option(i.text, i.value, undefined,
+                              i.value === selected);
       select.add(option);
     });
 
@@ -29,36 +27,32 @@ window.addEventListener('DOMContentLoaded', function () {
   }
 
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', ABS_BASE_URL + '/versions.json');
-  xhr.onload = function () {
+  xhr.open("GET", ABS_BASE_URL + "/versions.json");
+  xhr.onload = function() {
     var versions = JSON.parse(this.responseText);
 
-    var realVersion = versions.find(function (i) {
-      return (
-        i.version === CURRENT_VERSION || i.aliases.includes(CURRENT_VERSION)
-      );
+    var realVersion = versions.find(function(i) {
+      return i.version === CURRENT_VERSION ||
+             i.aliases.includes(CURRENT_VERSION);
     }).version;
 
-    var select = makeSelect(
-      versions.map(function (i) {
-        return { text: i.title, value: i.version };
-      }),
-      realVersion
-    );
-    select.addEventListener('change', function (event) {
-      window.location.href = ABS_BASE_URL + '/../' + this.value;
+    var select = makeSelect(versions.map(function(i) {
+      return {text: i.title, value: i.version};
+    }), realVersion);
+    select.addEventListener("change", function(event) {
+      window.location.href = ABS_BASE_URL_PART + "/" + this.value;
     });
 
-    var container = document.createElement('div');
-    container.id = 'custom_select';
-    container.classList.add('side-column-block');
+    var container = document.createElement("div");
+    container.id = "custom_select";
+    container.classList.add("side-column-block");
+
 
     // Add menu
     container.appendChild(select);
 
-    var sidebar = document.querySelector('#version-select-wrapper'); // Inject menu into element with this ID
+    var sidebar = document.querySelector("#version-select-wrapper"); // Inject menu into element with this ID
     sidebar.appendChild(container);
   };
-
   xhr.send();
 });
