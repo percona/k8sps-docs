55ee177c17e96a910b2632029deba9d33be7c371
Dima <dmitriykostiuk@gmail.com>
2022-10-31 21:58:22 +0300

    minor fix

--- docs/js/promptremover.js
+++ docs/js/promptremover.js
@@ -6,17 +6,21 @@ document.addEventListener("DOMContentLoaded", function(){
         const commandElement=collection.item(i);
         //const commandText=commandTextElement.innerHTML;
         //console.log(commandElement);
-        let commandCodeElement = commandElement.getElementsByTagName("code");
+
+        let commandButtonElement = commandElement.getElementsByTagName("button");
+        let promptString = commandButtonElement.item(0).getAttribute("data-prompt");
         //console.log(commandCodeElement);
+        if (!promptString) return;
+        let commandCodeElement = commandElement.getElementsByTagName("code");
         let commandCodeElementString = commandCodeElement.item(0).textContent;
         //console.log(commandCodeElementString);
         let trueCommand = commandCodeElementString;
-        if (commandCodeElementString.startsWith("$")) {
-            trueCommand = commandCodeElementString.substring(2, commandCodeElementString.length).trim();
+        if (commandCodeElementString.startsWith(promptString)) {
+            trueCommand = commandCodeElementString.substring(promptString.length, commandCodeElementString.length).trim();
         }
-        console.log(trueCommand);
-        let commandButtonElement = commandElement.getElementsByTagName("button");
-        console.log(commandButtonElement);
+        //console.log(trueCommand);
+        
+        //console.log(commandButtonElement);
         commandButtonElement.item(0).setAttribute("data-clipboard-text", trueCommand);
     }
 });
