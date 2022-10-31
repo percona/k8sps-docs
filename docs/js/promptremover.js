23885dc5e872ea09cbb5cab5f7bf36d56ac34fc1
Dmitriy Kostiuk <dmitriy.kostiuk@percona.com>
2022-11-01 00:31:38 +0300

    Update promptremover.js
    minor refinement

--- docs/js/promptremover.js
+++ docs/js/promptremover.js
@@ -4,18 +4,17 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let i = 0; i < collection.length; i++) {
         const commandElement=collection.item(i);
         let commandButtonElement = commandElement.getElementsByTagName("button");
-        // read prompt string from an attribute of the code block:
+        // read the prompt string from an attribute of the code block:
         let promptString = commandElement.getAttribute("data-prompt");
         if (!promptString) continue;
         let commandCodeElement = commandElement.getElementsByTagName("code");
         let commandCodeElementString = commandCodeElement.item(0).textContent;
-        //console.log(commandCodeElementString);
         let trueCommand = commandCodeElementString;
         if (commandCodeElementString.startsWith(promptString)) {
-        // remove first occurence of the prompt:
+        // remove the first occurrence of the prompt:
             trueCommand = commandCodeElementString.substring(promptString.length, commandCodeElementString.length).trim();
         }
-        // remove other occurencies in case of a multi-line string:
+        // remove other occurrencies in case of a multi-line string:
         trueCommand = trueCommand.replaceAll("\n"+promptString, "\n").replace(/^[^\S\r\n]+/gm, "");
 
         // CHECK IF THERE IS A SECOND PROMPT:
@@ -35,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function(){
             }
             trueCommand = trueCommand.replaceAll("\n"+promptString, "\n").replace(/^[^\S\r\n]+/gm, "");
         }
-        // attach updated command as an attribute to the button where clipboard.js will find it:
+        // attach the updated command as an attribute to the button where clipboard.js will find it:
         commandButtonElement.item(0).setAttribute("data-clipboard-text", trueCommand);
     }
 });
